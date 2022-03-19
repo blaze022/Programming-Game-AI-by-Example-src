b368383261bb83ea410cebddaba61f766cac3c80
blaze022 <95673987+blaze022@users.noreply.github.com>
2022-03-19 21:01:49 +0530

    Update BaseGameEntity.cpp

--- Buckland_Chapter2-State Machines/WestWorld1/BaseGameEntity.cpp
+++ Buckland_Chapter2-State Machines/WestWorld1/BaseGameEntity.cpp
@@ -18,6 +18,21 @@ void BaseGameEntity::SetID(int val)
 {
   //make sure the val is equal to or greater than the next available ID
   assert ( (val >= m_iNextValidID) && "<BaseGameEntity::SetID>: invalid ID");
+  
+  /*
+  
+
+This is a common trick when using assert macro from <cassert>. The macro fails if its argument evaluates to false, then the program exits without any readable diagnostic message (usually just a line number, file and stringified condition of the assert()).
+
+That is, to improve the diagnostic message, one can hack the boolean expression by adding a string literal that will be printed out together with the condition:
+
+assert((val >= m_iNextValidID) && "<BaseGameEntity::SetID>: invalid ID");
+//                                 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+The string literal always evaluates to boolean true (because it is a pointer), hence, it makes no impact to the condition itself. However, when the first operand of && condition fails, then the entire condtion is not satisfied (due to its logic) and assert ends the program, printing out:
+
+Assertion failed: (val >= m_iNextValidID) && "<BaseGameEntity::SetID>: invalid ID"
+Then, seeing this message, one can know that the provided ID for SetID method was not valid.
+*/
 
   m_ID = val;
     
